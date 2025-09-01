38a4b3b7fdbe1a75e1788d5ce3df7ef3b7874fc7
PRINCIPAL <PRINCIPAL@PORTATIL>
2025-08-31 22:29:18 -0500

    carga inicial

---
+++ src/main/java/Programa1_Busqueda2D.java
@@ -0,0 +1,67 @@
+public class Programa1_Busqueda2D {
+    
+    public static void main(String[] args) {
+        // Declarar una matriz 3x3 de enteros
+        int[][] matriz = {
+            {10, 25, 7},
+            {14, 33, 18},
+            {9, 42, 31}
+        };
+        
+        // Mostrar la matriz
+        System.out.println("Matriz 3x3:");
+        mostrarMatriz(matriz);
+        System.out.println();
+        
+        // Buscar varios valores para demostrar el funcionamiento
+        int[] valoresBuscar = {25, 42, 99, 10, 50};
+        
+        for (int valor : valoresBuscar) {
+            int[] posicion = buscarEnMatriz(matriz, valor);
+            
+            if (posicion[0] == -1 && posicion[1] == -1) {
+                System.out.println("El valor " + valor + " NO se encontró en la matriz.");
+            } else {
+                System.out.println("El valor " + valor + " se encontró en la posición [" + 
+                                 posicion[0] + ", " + posicion[1] + "]");
+            }
+        }
+    }
+    
+    /**
+     * Busca un valor específico en una matriz 2D
+     * @param matriz La matriz donde buscar
+     * @param valor El valor a buscar
+     * @return Un arreglo con la posición [fila, columna] o [-1, -1] si no se encuentra
+     */
+    public static int[] buscarEnMatriz(int[][] matriz, int valor) {
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                if (matriz[fila][columna] == valor) {
+                    return new int[]{fila, columna};
+                }
+            }
+        }
+        // Si no se encuentra, retornar [-1, -1]
+        return new int[]{-1, -1};
+    }
+    
+    /**
+     * Método auxiliar para mostrar la matriz en consola
+     * @param matriz La matriz a mostrar
+     */
+    public static void mostrarMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("%3d ", matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+       
+     
+
+
+
